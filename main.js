8f4d65576ec38645363a8589d25159eea274e4be
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-11-07 23:43:01 +0530

    third commit

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ console.log(myLet);
 }
 console.log(myVar2);
 console.log(myLet);
+
+/*Binary numbers*/
+console.log(0b1010,typeof 0b10101)
+console.log(0b1010n,typeof 0b10101n)
+
+/*Ocatal numbers*/
+console.log(0o10,typeof 0o10) 
+console.log(0o10n,typeof 0o10n) 
+
+/* HexaDecimal numbers*/
+console.log(0xFF,typeof 0xFF)
+console.log(0xFFn,typeof 0xFFn)
+
+/*Decimal numbers */
+console.log(10)
+console.log(1250.25)
+console.log(1.256e2, 1.245E4)
