3257721d3db342620a94df3901cdfcdd4ac41b7b
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-11-07 23:41:01 +0530

    second commit

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello Ecma Script");
+console.log(null==undefined)
+console.log(null===undefined)
+console.log(true,false)
+
+console.log("Hello");
+// console.log(myVar);
+var myVar = 10;
+let myLet= 2;
+console.log(myVar);
+console.log(myLet);
+{
+    // console.log(myLet);
+    var myVar2 = "abc";
+    let myLet = "IJSE";
+    console.log(myLet);
+}
+console.log(myVar2);
+console.log(myLet);
